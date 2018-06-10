ad26b7bb917fea3f03ac37a2c171856e802c3d92
Diouck Abdou <diouckk@hotmail.com>
2018-06-10 09:53:43 +0200

    Add Publicite Google

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -463,6 +463,12 @@
 
    @yield('scripts')
 
-
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-2958656141868069",
+    enable_page_level_ads: true
+  });
+</script>
 
 </html>
